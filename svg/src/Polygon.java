52057ed622874d2bf75b26ae53e67da916188391
kar <nig@nig>
2025-03-13 16:21:18 +0100

    commit_niedziolka

--- svg/src/Polygon.java
+++ svg/src/Polygon.java
@@ -3,13 +3,19 @@ public class Polygon {
     private final Point[] vertices;
     private final Style style;
 
-    public static Segment maxSegment (Segment diagonal, Style style){
-        Segment[] perpendiculars = diagonal.perpendicularSegment(
+    public static Polygon square(Segment diagonal, Style style){
+        Segment[] perpendiculars = diagonal.perpendicularSegments(
                 diagonal.getCenter(), diagonal.length()/2
         );
-
+        return new Polygon(new Point[]{
+                diagonal.getA(),
+                perpendiculars[0].getB(),
+                diagonal.getB(),
+                perpendiculars[1].getB()
+        }, style);
     }
-    // konstruktor dokonuje głębokiej kopii tablic
+
+    // konstruktor dokonuje głębokiej kopii tablicy
     public Polygon(Point[] vertices) {
         this(vertices, new Style("none", "black", 1));
     }
